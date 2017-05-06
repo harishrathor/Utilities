0084029b862560f10b7a0387148a3031370401a3
harishrathor <harishrathor95@gmail.com>
2017-05-06 09:41:16 +0530

    PHP Utility for creating directories using json.
    I works for both, windows and linux  just by changing directory separator in the code

--- PHP Utilities/createDir.php
+++ PHP Utilities/createDir.php
@@ -1,12 +1,13 @@
 <?php
 
 class DirUtility{
-	private $cur_dir,$full_root_path,$parent_dir;
+	private $cur_dir,$full_root_path,$parent_dir,$separator;
 
-	function createDirs($full_root_path,$json){
+	function createDirs($full_root_path,$separator,$json){
 		$this->full_root_path=$full_root_path;
 		$this->parent_dir=$full_root_path;
 		$parsed_json=json_decode($json);
+		$this->separator=$separator;
 		$parsed_json_array=(array)$parsed_json;
 		$this->makeDirs('',$parsed_json_array,'');
 	}
@@ -14,10 +15,10 @@ class DirUtility{
 	function makeDirs($key_as_fol_name,$val_as_nested_dir,$parent_dir){
 
 		if($key_as_fol_name)
-			$this->cur_dir=$parent_dir.'/'.$key_as_fol_name;
+			$this->cur_dir=$parent_dir.$this->separator.$key_as_fol_name;
 
 		if($this->cur_dir)	
-			if(!mkdir($this->full_root_path.$this->cur_dir,0777,true)){
+			if(!mkdir($this->full_root_path.$this->cur_dir,true)){
 					print("Failed to create dir ".$this->cur_dir." because of ");
 					print_r(error_get_last());
 				}
@@ -41,6 +42,9 @@ class DirUtility{
 	}
 	$full_root_path=getcwd();
 	$json=file_get_contents("file.json");
+	
+	$separator="\\"; // "\" for windows and "/" for linux
+
 	$cr_dir=new DirUtility();
-	$cr_dir->createDirs($full_root_path,$json);
+	$cr_dir->createDirs($full_root_path,$separator,$json);
 	?>
